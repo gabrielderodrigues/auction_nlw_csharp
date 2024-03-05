2179d8ac9a862372494d395d40714e307e4c58c9
Gabriel Oliveira de Lima Rodrigues <g.de.lima.rodrigues@avanade.com>
2024-03-05 14:24:22 -0300

    Add project files.

---
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,14 @@
+﻿using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    public Auction Execute()
+    {
+        var repository = new RocketseatAuctionDbContext();
+
+        return repository.Auctions.First();
+    }
+}
