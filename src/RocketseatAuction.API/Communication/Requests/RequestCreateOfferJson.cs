dd22d618a15be57a320892e1579279977d500ca4
Gabriel Oliveira de Lima Rodrigues <g.de.lima.rodrigues@avanade.com>
2024-03-05 17:13:26 -0300

    feat: Criação da controller RocketseatAuctionBaseController e criação do enum Condition

---
+++ src/RocketseatAuction.API/Communication/Requests/RequestCreateOfferJson.cs
@@ -0,0 +1,8 @@
+﻿namespace RocketseatAuction.API.Communication.Requests;
+
+public class RequestCreateOfferJson
+{
+    public decimal Price { get; set; }
+    public string Name { get; set; } = string.Empty
+    public string Description { get; set; } = string.Empty;
+}
---
+++ src/RocketseatAuction.API/Controllers/OfferController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Communication.Requests;
+
+namespace RocketseatAuction.API.Controllers;
+
+public class OfferController : RocketseatAuctionBaseController
+{
+    [HttpPost]
+    [Route("{itemId}")]
+    public IActionResult CreateOffer([FromRoute] int itemId, [FromBody] RequestCreateOfferJson requestCreateOfferJson)
+    {
+        return Created();
+    }
+}
