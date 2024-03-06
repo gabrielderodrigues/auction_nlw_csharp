175e58fa82ce50195aafc53c9f53b1b17c5c3e90
Gabriel Oliveira de Lima Rodrigues <g.de.lima.rodrigues@avanade.com>
2024-03-06 13:31:09 -0300

    feat: Criação das entidades Offer e User; criação da OfferController; Implementação da autenticação dos requests.

--- src/RocketseatAuction.API/Communication/Requests/RequestCreateOfferJson.cs
+++ src/RocketseatAuction.API/Communication/Requests/RequestCreateOfferJson.cs
@@ -3,6 +3,6 @@
 public class RequestCreateOfferJson
 {
     public decimal Price { get; set; }
-    public string Name { get; set; } = string.Empty
+    public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
 }
--- src/RocketseatAuction.API/Controllers/OfferController.cs
+++ src/RocketseatAuction.API/Controllers/OfferController.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RocketseatAuction.API.Communication.Requests;
+using RocketseatAuction.API.Filters;
+using RocketseatAuction.API.UseCases.Offers.CreateOffer;
 
 namespace RocketseatAuction.API.Controllers;
 
+[ServiceFilter(typeof(AuthenticationUserAttribute))]
 public class OfferController : RocketseatAuctionBaseController
 {
     [HttpPost]
     [Route("{itemId}")]
-    public IActionResult CreateOffer([FromRoute] int itemId, [FromBody] RequestCreateOfferJson requestCreateOfferJson)
+    public IActionResult CreateOffer(
+        [FromRoute] int itemId,
+        [FromBody] RequestCreateOfferJson requestCreateOfferJson,
+        [FromServices] CreateOfferUseCase useCase)
     {
-        return Created();
+        var id = useCase.Execute(itemId, requestCreateOfferJson);
+        return Created(string.Empty, id);
     }
 }
